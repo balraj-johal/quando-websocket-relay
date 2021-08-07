769c3b5902711a47fbe4da40111f915864be3d3a
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-08-07 16:17:03 +0100

    init basic websocket server

---
+++ server.js
@@ -0,0 +1,24 @@
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const { Server } = require('ws');
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//create new websocket server
+const wss = new Server({ server });
+
+// begin listening for websocket connections
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+//broadcast updates per second
+setInterval(() => {
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 1000);
